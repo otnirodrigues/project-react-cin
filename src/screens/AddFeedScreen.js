168eb160ed1adbae544c56cd29de6a580f5ffa84
@otni.rodrigues <ors@cin.ufpe.br>
2023-05-12 16:03:18 -0300

    implementando a chamada do formFeed via componente

--- src/screens/AddFeedScreen.js
+++ src/screens/AddFeedScreen.js
@@ -3,9 +3,10 @@ import { Text, StyleSheet } from 'react-native';
 
 const AddFeedScreen = ({ navigation }) => {
     return (
-        <Text>Implementar formulário de adição de RSS feeds, passando título e link. Demais informações devem ser obtidas do arquivo XML (tag channel), como imagem, descrição, etc.</Text>
-    );
-};
+        // adicionando os feeds via componente
+        // chamando o formfeed
+        <FormFeed nave = {navigation}/>
+)};
 
 const styles = StyleSheet.create({});
 
